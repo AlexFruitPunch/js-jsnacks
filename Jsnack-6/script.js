af50e200752b4c519a4a7d5a1326d23d943a4b73
alexpagani <alexpaganibr@gmail.com>
2022-03-25 16:03:36 +0100

    Sesto esercizio Terminato

--- Jsnack-6/script.js
+++ Jsnack-6/script.js
@@ -11,21 +11,25 @@ alert("Uscirai solo finche non vincerai");
 
 //inizio gioco
 while(risultato == false){
-//generazione numero randomico da 0 a 10 con controlli 
-NumeroPC = math.floor(math.random() * 10);
-console.log("il numero del PC è" + NumeroPC);
+    //generazione numero randomico da 0 a 10 con controlli 
+    NumeroPC = Math.floor(Math.random() * 10);
+    console.log("il numero del PC è " + NumeroPC);
 
-alert("Ho già estratto il mio numero");
-//utente ci dice che numero vuole usare
-NumeroUtente = parseInt(prompt("Dimmi un numero da 0 a 10"));
-console.log("il numero dell'utente è" + NumeroUtente);
-
-if(NumeroPC == NumeroUtente){
-    alert("hai vinto !!!");
-    risultato = true;
-    document.getElementById("Esito").innerHTML = "hai vinto !!!"; 
-} else{
-    alert("hai perso mi dispiace, ritenta e sarai più fortunato");
-    document.getElementById("Esito").innerHTML = "hai perso mi dispiace !!!";
-}
+    alert("Ho già estratto il mio numero");
+    //utente ci dice che numero vuole usare
+    NumeroUtente = parseInt(prompt("Dimmi un numero da 0 a 10"));
+    console.log("il numero dell'utente è " + NumeroUtente);
+    
+    if(NumeroUtente >= 0 && NumeroUtente <= 10) {
+        if(NumeroPC == NumeroUtente){
+            alert("hai vinto !!!");
+            risultato = true;
+            document.getElementById("Esito").innerHTML = "hai vinto !!!"; 
+        } else{
+        alert("hai perso mi dispiace, ritenta e sarai più fortunato");
+        document.getElementById("Esito").innerHTML = "hai perso mi dispiace !!!";
+        }
+    } else{
+        alert("non va bene, dev'essere un numero compreso tra 0 e 10");
+    }    
 }
