44f5117a220f47489368bb086d1a42113ffd35ad
alexpagani <alexpaganibr@gmail.com>
2022-03-28 10:10:24 +0200

    Piccola correzione in classe

--- Jsnack-9/script.js
+++ Jsnack-9/script.js
@@ -1,21 +1,24 @@
 //dichiarazione array vuoto
-let Numeri = [];
+let ArrayNumero = [];
+let Numero;
 
-let Risultato = 0;
+let Summ = 0;
 let Mediaritm = 0;
 
 //caricamento dell'array
-for(i = 0; i < 10; i++){
-    Numeri[i] = parseInt(prompt("inserisci un numero"));
+for(let i = 0; i < 10; i++){
+    Numero = parseInt(prompt("inserisci un numero"));
+    ArrayNumero.push(Numero);
 }
 
-for(i = 0; i < 10; i++){
-    Risultato = Risultato + Numeri[i];
+for(let i = 0; i < 10; i++){
+    Summ = Summ + ArrayNumero[i];
 }
 
-Mediaritm = mediaAritmetica(Risultato, Numeri.length);
+Mediaritm = mediaAritmetica(Summ, ArrayNumero.length);
 alert("la media aritmetica dei tuoi numeri è: " + Mediaritm);
 document.getElementById("MediArray").innerHTML = Mediaritm;
+
 //Funzioni
 
 function mediaAritmetica(Num, lunghezza) {
